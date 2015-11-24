634de92b6a375848a2d041a9a7cea084dd571f36
Kiril Hristov <kirilhristov91@gmail.com>
2015-11-24 21:12:12 +0000

    add nextBike stands

--- app/src/main/java/com/hci4/MapsActivity.java
+++ app/src/main/java/com/hci4/MapsActivity.java
@@ -36,13 +36,52 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     private Button mapsButton;
     private String username;
 
-
-    private static final LatLng LOWER_MANHATTAN = new LatLng(40.722543, -73.998585);
-    private static final LatLng BROOKLYN_BRIDGE = new LatLng(40.7057, -73.9964);
-    private static final LatLng WALL_STREET = new LatLng(40.7064, -74.0094);
-
+    // hardcoded points for directions
     private static final LatLng UNI = new LatLng(55.8719, -4.2875);
     private static final LatLng Home = new LatLng(55.8691926, -4.2761092);
+
+    // nextbike stands
+    private static final LatLng stand_Central_Station = new LatLng(55.86078768935794, -4.258602261543274);
+    private static final LatLng stand_Waterloo_Street = new LatLng(55.860767, -4.264083);
+    private static final LatLng stand_Queen_Street_Railway_Station = new LatLng(55.861967, -4.252283);
+    private static final LatLng stand_George_Square = new LatLng(55.86155, -4.2494);
+    private static final LatLng stand_Merchant_Square = new LatLng(55.858167, -4.245483);
+    private static final LatLng stand_St_George_Cross = new LatLng(55.871367, -4.269117);
+    private static final LatLng stand_St_Enoch_Square = new LatLng(55.85682862583277, -4.2552924156188965);
+    private static final LatLng stand_Glasgo_Cathedral = new LatLng(55.862883, -4.237467);
+    private static final LatLng standGlasgowGreen = new LatLng(55.84945, -4.23375);
+    private static final LatLng stand_Broomielaw = new LatLng(55.85665, -4.26375);
+    private static final LatLng stand_SECC = new LatLng(55.85912884568613, -4.2867279052734375);
+    private static final LatLng stand_Riverside_Museum = new LatLng(55.865683, -4.305367);
+    private static final LatLng stand_Butanic_Gardens = new LatLng(55.87827800626413, -4.288487434387207);
+    private static final LatLng stand_KelvinBridge_Subway = new LatLng(55.87505, -4.28035);
+    private static final LatLng stand_Kelvingrove_Art_Gallery = new LatLng(55.868283, -4.28945);
+    private static final LatLng stand_Charing_Cross_Railway_Station = new LatLng(55.8646, -4.27005);
+    private static final LatLng stand_University_of_Glasgow_West = new LatLng(55.8728, -4.2927);
+    private static final LatLng stand_Mitchell_Library = new LatLng(55.864788492724344, -4.272061586380005);
+    private static final LatLng stand_University_of_Strathclyde_North = new LatLng(55.862983, -4.241);
+    private static final LatLng stand_Bridge_Street_Subway = new LatLng(55.8525, -4.25885);
+    private static final LatLng stand_Argyle_Street_Railway_Station = new LatLng(55.857567, -4.249717);
+    private static final LatLng stand_Glasgow_Caledonian_University = new LatLng(55.865817, -4.251367);
+    private static final LatLng stand_Emirates_Arena = new LatLng(55.846367, -4.208);
+    private static final LatLng stand_Gallery_of_Modern_Art = new LatLng(55.859733, -4.251917);
+    private static final LatLng stand_Trongate = new LatLng(55.856083, -4.24465);
+    private static final LatLng stand_University_of_Strathclyde_South = new LatLng(55.860633, -4.2419);
+    private static final LatLng stand_University_of_Glasgow_East = new LatLng(55.87165, -4.27795);
+    private static final LatLng stand_Partick_Interchange = new LatLng(55.86995, -4.308433);
+    private static final LatLng stand_Glasgow_Science_Centre = new LatLng(55.8585, -4.292517);
+    private static final LatLng stand_Finnieston_Street = new LatLng(55.85871939242914, -4.281738996505737);
+    private static final LatLng stand_Bellgrove_Railway_Station_North = new LatLng(55.858227, -4.224286);
+    private static final LatLng stand_Barrowlands = new LatLng(55.855247, -4.235172);
+    private static final LatLng stand_Bridgeton_Cross = new LatLng(55.848881, -4.226744);
+    private static final LatLng stand_City_of_Glasgow_College_Riverside_Campus = new LatLng(55.850554, -4.248127);
+    private static final LatLng stand_Paisley_Road_Toll = new LatLng(55.85378, -4.279158);
+    private static final LatLng stand_Cessnock_Subway_Station = new LatLng(55.851964, -4.294308);
+    private static final LatLng stand_Govan_Cross = new LatLng(55.863331, -4.311153);
+    private static final LatLng stand_Eglinton_Toll = new LatLng(55.842696, -4.262674);
+    private static final LatLng stand_Queens_Park_Railway_Station = new LatLng(55.834567,-4.265429);
+    private static final LatLng stand_Queens_Park_West = new LatLng(55.835022,-4.272439);
+    private static final LatLng stand_Buchanan_Street_Bus_Station = new LatLng(55.864986,-4.252261);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -65,15 +104,19 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                 Intent intent = new Intent(MapsActivity.this, MainActivity.class);
                 intent.putExtra("username", username);
                 startActivity(intent);
+                finish();
             }
         });
 
         String route = makeURL(UNI.latitude, UNI.longitude, Home.latitude,Home.longitude);
         Log.i("URL_LINK za serveRA ", route);
         new connectAsyncTask(route).execute();
-        mMap.addMarker(new MarkerOptions().position(UNI).title("manhanta"));
-        mMap.addMarker(new MarkerOptions().position(Home).title("manhanta"));
+        mMap.addMarker(new MarkerOptions().position(UNI).title("Uni"));
+        mMap.addMarker(new MarkerOptions().position(Home).title("Home"));
         mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(UNI, 10));
+
+
+
 
         mMap.setMyLocationEnabled(true);
         Location myLoc = mMap.getMyLocation();
