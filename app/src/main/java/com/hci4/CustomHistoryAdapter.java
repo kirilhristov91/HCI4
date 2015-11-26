6832c7fa1335df8a036f081437f0990084859ceb
Kiril Hristov <kirilhristov91@gmail.com>
2015-11-26 00:24:07 +0000

    update history list performance

--- app/src/main/java/com/hci4/CustomHistoryAdapter.java
+++ app/src/main/java/com/hci4/CustomHistoryAdapter.java
@@ -10,42 +10,65 @@ import android.widget.TextView;
 import android.widget.ImageView;
 
 import java.util.ArrayList;
+import java.util.Objects;
 
 class CustomHistoryAdapter extends ArrayAdapter<String> {
 
     ArrayList<History> historyItems;
+    String[] from;
     public CustomHistoryAdapter(Context context, String[] from, ArrayList<History> items) {
         super(context, R.layout.custom_row, from);
         historyItems = items;
+        this.from = from;
+    }
+
+    static class ViewHolderItem{
+        TextView textview8;
+        TextView textview10;
+        TextView textview12;
+        TextView textview14;
+        ImageView imageViewRow;
     }
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
 
-        LayoutInflater inflater = LayoutInflater.from(getContext());
-        View customView = inflater.inflate(R.layout.custom_row, parent, false);
+        ViewHolderItem viewHolder;
 
-        TextView textView8 = (TextView) customView.findViewById(R.id.textView8);
-        TextView textView10 = (TextView) customView.findViewById(R.id.textView10);
-        TextView textView12 = (TextView) customView.findViewById(R.id.textView12);
-        TextView textView14 = (TextView) customView.findViewById(R.id.textView14);
-        ImageView imageViewRow = (ImageView) customView.findViewById(R.id.imageViewRow);
+        if(convertView == null) {
 
+            LayoutInflater inflater = LayoutInflater.from(getContext());
+            convertView = inflater.inflate(R.layout.custom_row, parent, false);
 
-        textView8.setText(historyItems.get(position).getFrom());
-        textView10.setText(historyItems.get(position).getDestination());
+            viewHolder = new ViewHolderItem();
 
-        double cost = Math.random()*11;
-        String text = String.format("%.2f", cost)+"£";
-        textView12.setText(text);
+            viewHolder.textview8 = (TextView) convertView.findViewById(R.id.textView8);
+            viewHolder.textview10 = (TextView) convertView.findViewById(R.id.textView10);
+            viewHolder.textview12 = (TextView) convertView.findViewById(R.id.textView12);
+            viewHolder.textview14 = (TextView) convertView.findViewById(R.id.textView14);
+            viewHolder.imageViewRow = (ImageView) convertView.findViewById(R.id.imageViewRow);
 
-        textView14.setText(historyItems.get(position).getDate());
-
-        if (historyItems.get(position).getChoice().equals("car")) {
-            imageViewRow.setImageResource(R.drawable.car);
+            convertView.setTag(viewHolder);
+        }else{
+            viewHolder = (ViewHolderItem) convertView.getTag();
         }
-        else imageViewRow.setImageResource(R.drawable.bike);
 
-        return customView;
+        String fromDestionation = from[position];
+
+        if(fromDestionation != null){
+            viewHolder.textview8.setText(historyItems.get(position).getFrom());
+            viewHolder.textview10.setText(historyItems.get(position).getDestination());
+
+            double cost = Math.random() * 11;
+            String text = String.format("%.2f", cost) + "£";
+            viewHolder.textview12.setText(text);
+            viewHolder.textview14.setText(historyItems.get(position).getDate());
+
+            if (historyItems.get(position).getChoice().equals("car")) {
+                viewHolder.imageViewRow.setImageResource(R.drawable.car);
+            } else viewHolder.imageViewRow.setImageResource(R.drawable.bike);
+        }
+
+        return convertView;
     }
 }
